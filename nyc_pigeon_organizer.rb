952c7754ab72c8b8ddb78393d23cf0ac6bfa59a8
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-25 18:01:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-final_results = data.each_with_object({}) do | (key, value), fa |
-  value.each do |inner_keys, names|
+final_results = data.each_with_object({}) do | (k, v), fa |
+  v.each do |k2, names|
     names.each do |name|
       if !fa[name]
         fa[name] = {}
       end
-      if !fa[name][key]
-        !fa[name][key] = []
+      if !fa[name][k]
+        !fa[name][k] = []
       end
-      fa[name][key].push(inner_keys.to_s)
+      fa[name][k].push(k2.to_s)
     end
   end
   end
